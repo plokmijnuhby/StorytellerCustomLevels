f86eb4daad9e45e72185c19290f4d79119f7bd23
Dominic Littlewood <11dlittlewood@gmail.com>
2023-11-18 20:50:52 +0000

    Remove debug function

--- src/DebugTools.cs
+++ src/DebugTools.cs
@@ -7,33 +7,6 @@ namespace CustomLevels;
 internal class DebugTools
 {
     // Debug functions. A release version of this mod should never run these!
-    public static void DumpAllTextures()
-    {
-        RenderTexture prevRender = RenderTexture.active;
-        foreach (var animation in ResourceLoader.animationCache.Keys)
-        {
-            var texture = ResourceLoader.GetSprite(animation).texture;
-
-            // You'd think we'd be done here, but actually the old texture isn't readable;
-            // the CPU component has been deleted, and only the GPU component remains.
-            // To read it, we need to render it, then effectively take a screenshot,
-            // creating a texture that retains its CPU component.
-            var render = RenderTexture.GetTemporary(
-                texture.width, texture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear
-                );
-            Graphics.Blit(texture, render);
-            RenderTexture.active = render;
-            var newTexture = new Texture2D(texture.width, texture.height);
-            newTexture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
-            newTexture.Apply();
-            RenderTexture.ReleaseTemporary(render);
-
-            // Now we just have to write it.
-            Console.WriteLine($"Dumping {animation}");
-            File.WriteAllBytes($"output/{animation}.png", ImageConversion.EncodeToPNG(newTexture));
-        }
-        RenderTexture.active = prevRender;
-    }
 
     public static void DumpEnums()
     {
