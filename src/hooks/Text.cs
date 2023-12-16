a6a5dadfc55016e6b614aec254e8957179845a3d
Dominic Littlewood <11dlittlewood@gmail.com>
2023-12-16 12:42:03 +0000

    Move actor file

--- src/hooks/Text.cs
+++ src/hooks/Text.cs
@@ -8,15 +8,11 @@ internal class Text_GetActorLabel
 {
     static bool Prefix(ActorId id, ref string __result)
     {
-        foreach (string file in Directory.GetFiles("./custom_levels", "*.actors", SearchOption.AllDirectories))
+        foreach (string line in File.ReadLines("./custom_levels/actors.txt"))
         {
-            foreach (string line in File.ReadLines(file))
+            string[] lineParts = line.Split();
+            if (lineParts.Length >= 2 && lineParts[0] == id.ToString())
             {
-                string[] lineParts = line.Split();
-                if (lineParts[0] != id.ToString())
-                {
-                    continue;
-                }
                 __result = lineParts[1];
                 return false;
             }
